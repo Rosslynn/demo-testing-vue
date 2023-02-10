d2d4c285a4955887069a621c810b8fc4c579d437
Lynross <mauriciobarva@gmail.com>
2023-02-09 23:43:05 -0500

    test: AnimeInformation tests updated

--- src/__tests__/AnimeInformationScaffold.spec.js
+++ src/__tests__/AnimeInformationScaffold.spec.js
@@ -1,3 +1,4 @@
 describe('AnimeInformation', () => {
   it('should render the anime information', () => {});
+  it('if the background of the anime is not available should render N/A', () => {});
 });
